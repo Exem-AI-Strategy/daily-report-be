424d8a401a7f6ab403907f57479e4d8a3edad048
yeoeun-ex <yeoeun.sim@ex-em.com>
2025-08-26 15:22:30 +0900

    feat: signin request, response dto 생성

---
+++ src/main/java/com/ai/dailyReport/auth/dto/SignInResponseDto.java
@@ -0,0 +1,20 @@
+package com.ai.dailyReport.auth.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class SignInResponseDto {
+    private String status;
+    private UserData data;
+    private String token;
+    
+    @Data
+    @Builder
+    public static class UserData {
+        private Long userId;
+        private String email;
+        private String role;
+    }
+}
